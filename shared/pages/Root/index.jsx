381b9e7dd238e8c1e8b01f3ce5cecb37209afb84
Terence Ge <terencegehui@yahoo.com>
2025-06-18 20:46:06 +0800

    add chat page

--- shared/pages/Root/index.jsx
+++ shared/pages/Root/index.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect, useState } from 'react'
 import classNames from 'classnames'
 import { createPortal } from 'react-dom'
-import { Outlet } from 'react-router'
+import { Outlet, useLocation } from 'react-router'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { withRouter } from 'utils/withRouter'
@@ -14,18 +14,21 @@ import styles from './style.css'
 
 const Root = ({ location, history, actions }) => {
   const [title, setTitle] = useState('Agent Meter')
+  const currentLocation = useLocation()
 
   useEffect(() => {
     setTitle('Agent Meter')
   }, [])
 
+  const isChatPage = currentLocation.pathname === '/chat'
+
   return (
     <Fragment>
       <Title render={title} />
       <div className={classNames(styles.root)}>
-        <Header />
+        {!isChatPage && <Header />}
         <Outlet />
-        <Footer />
+        {!isChatPage && <Footer />}
       </div>
     </Fragment>
   )
